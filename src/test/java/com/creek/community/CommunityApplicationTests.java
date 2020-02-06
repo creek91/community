92fa6ec799609f12e20cdf2e579c513d98fe9cfc
Creek <hebin@buaa.edu.cn>
2020-02-06 17:12:00 +0800

    init repo

---
+++ src/test/java/com/creek/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creek.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
